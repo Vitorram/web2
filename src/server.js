6102a8f2abe83b08d1b65392871f5f0dd2039016
Vitor Menezes <161902126+Vitorram@users.noreply.github.com>
2025-05-28 12:46:43 +0000

    aplicando o cors ao projeto

--- src/server.js
+++ src/server.js
@@ -2,11 +2,12 @@ import express from 'express';
 import dotenv from 'dotenv';
 import userRoutes from './routes/userRoutes.js';
 import postRoutes from './routes/postRoutes.js';
+import cors from 'cors';
 
 dotenv.config();
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 // Rotas MVC
 app.use('/users', userRoutes);
 app.use('/posts', postRoutes);
